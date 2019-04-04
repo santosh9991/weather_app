17dd84e1b661d8f2e46de29daf7e05d2c65f09b7
santosh kesireddy <kesireddy.santosh@ibm.com>
2019-04-04 12:22:45 -0500

    created controllers routers and tested

--- app.js
+++ app.js
@@ -1,48 +1,37 @@
-//MODEL
-weatherApp = angular.module('weatherApp',['ngRoute','ngResource']);
-//ROUTERS
-weatherApp.config(function($routeProvider){
-    //route provider provides the routes. It decides what to do based on the hash provided
-    //
-//    console.log($location.path())
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider){
+    //routeProvider service is available through dependency injection. It is available through ngRoute.
+    //It lets us specifty the routs. In otherwords, it will decide what to do when it sees a perticular route in the hash.
+    // it also matches the patterns
+//    $log.ingo(location.path())
     $routeProvider
-    .when('/',{
-        
-        templateUrl:'pages/home.html',
-        controller:"homeController"
+    .when(
+        //router checks for a perticular hash
+        '/',
+    {
+    //second argument is an object which primarily takes two keys: 1.templateUrl ,2. controller.
+    // TemplateUrl will point to the piece of html code to which it should redirect and controller controlles that corresponding html
+    templateUrl:'pages/home.htm',
+    controller:'homeController'
     })
-    .when('/forcast',{
-        templateUrl:"pages/forcast.htm",
-        controller:"forcastController"
-        
+    .when(
+        //router checks for a perticular hash
+        '/forcast',
+    {
+    //second argument is an object which primarily takes two keys: 1.templateUrl ,2. controller.
+    // TemplateUrl will point to the piece of html code to which it should redirect and controller controlles that corresponding html
+    templateUrl:'pages/forecast.htm',
+    controller:'forcastController'
     })
-    
 })
-//weatherApp.config(function($routeProvider){
-//    $routeProvider
-////    .when('/',function(){
-////        templateUrl:"pages/home.html",
-////        controller:"mainController"
-////    })
-//    .when('/forcast',function(){
-//        templateUrl:'pages/forcast.html',
-//        controller:'forcastController'
-//    })
-//});
-//weatherApp.config(function ($routeProvider) {
-//    $routeProvider
-//    .when('/',function(){
-////        templateUrl:'pages/home.html',
-////        controller:'homeController'
-//    })
-//});
-//
-////CONTROLLERS
-weatherApp.controller("homeController",["$scope","$log","$location",function($scope,$log,$location){
-    $log.info("home")
+myApp.controller('homeController', ['$scope', '$location','$log',function($scope,$location,$log) {
     
-}])
-//
-weatherApp.controller("forcastController",["$scope","$log",function($scope,$log){
+//    $log.info($location.path());
     
-}])
+}]);
+myApp.controller('forcastController', ['$scope', '$location','$log',function($scope,$location,$log) {
+    
+//    $log.info($location.path());
+    
+}]);
